b440e2a1240666e396f292f8beb1c9226bbc84db
Steve Lammers <stevenlammers12@gmail.com>
2011-11-02 08:19:19 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CalfApp::Application.config.secret_token = 'aa7f5d81579d707fee479de0260c104300bba874f06824b43de2871583af3b7915d23a0e89d1b84f8953dcb66eb16c7e6aeb0c62c8108e99871579e9e8763387'
